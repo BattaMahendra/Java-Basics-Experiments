d1d68f0c36cb08911105ae365623709bee977cd7
BattaMahendra <bmc26g@gmail.com>
2023-06-09 19:14:12 +0530

    basic experiments

--- Java-Basic-Experiments/lib/src/main/java/basic/experiments/miscellaneous/Predicates.java
+++ Java-Basic-Experiments/lib/src/main/java/basic/experiments/miscellaneous/Predicates.java
@@ -1,13 +1,22 @@
 package basic.experiments.miscellaneous;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import basic.experiments.pojos.Employee;
+import basic.experiments.populating.pojos.VariablesAndTechniques;
 
 
 
 public class Predicates {
 	
-//	public static Predicate<?> filterEmpByCompany= empList -> Employee
+	public static Predicate<List<Employee>>
+						filterEmpByCompany=empList->empList.stream()
+															.anyMatch(emp -> emp.getCompany().equals("Mphasis"));
+														
+			public static void main(String[] args) {
+			
+			}											
+												
 
 }
---
+++ Java-Basic-Experiments/lib/src/main/java/basic/experiments/miscellaneous/StreamsVsForEach.java
@@ -0,0 +1,38 @@
+package basic.experiments.miscellaneous;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import basic.experiments.pojos.Employee;
+import basic.experiments.populating.pojos.VariablesAndTechniques;
+
+public class StreamsVsForEach {
+	
+	public static void main(String[] args) {
+		
+		List<Employee> empListNew=new ArrayList<>();
+		
+		List<Employee> empListOld=VariablesAndTechniques.populateEmployee();
+		try {
+		empListOld.stream().forEach(emp ->{
+			empListOld.clear();
+			empListNew.add(emp);
+			
+		} );
+		}catch (Exception e) {
+			System.out.println("inexception block");
+			 empListOld.forEach(System.out::println);
+			 System.out.println("end of exception block");
+		}
+//		empListOld=VariablesAndTechniques.populateEmployee();
+		
+		System.out.println("\n new List");
+		
+		empListNew.forEach(System.out::println);
+		
+		System.out.println("\n with for each");
+		
+		
+	}
+
+}
