261e175d7c5c18742fdab9a7237107c362222a7b
BattaMahendra <bmc26g@gmail.com>
2024-02-04 12:25:32 +0530

    method oveloading with Autoboxing

---
+++ src/main/java/AutoBoxing/MethodOverloading.java
@@ -0,0 +1,37 @@
+package AutoBoxing;
+
+public class MethodOverloading {
+
+	public static void print(int a) {
+		System.out.println("primitive");
+	}
+	
+	public static void print(Integer a) {
+		System.out.println("Integer wrapper");
+	}
+	
+	public static void print(Integer... a) {
+		System.out.println("var args");
+	}
+	
+	/*
+	 * we can observe that 
+	 * widening >> Autoboxing >> var args
+	 */
+	public static void main(String[] args) {
+		int a =5;
+		Integer b = new Integer(6);
+		Integer c =7;
+		short d =8;
+		byte e =9;
+		long f= 10;
+		print(a);
+		print(b);
+		print(c);
+		print(d);
+		print(e);
+		//print(f);
+	}
+	
+	
+}
