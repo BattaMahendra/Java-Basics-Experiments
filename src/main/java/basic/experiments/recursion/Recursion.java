92f677cd97ce393a5c2df0652c0e7d197251b9a3
BattaMahendra <BMC26G@GMAIL.COM>
2024-04-14 18:36:14 +0530

    Learning about main() method in java

--- src/main/java/basic/experiments/recursion/Recursion.java
+++ src/main/java/basic/experiments/recursion/Recursion.java
@@ -3,7 +3,17 @@ package basic.experiments.recursion;
 public class Recursion {
 
     public static void main(String[] args) {
-
-        main(args);
+        /*
+        *
+        * You can provide arguments in the run configuration of the program
+        * JVM will load whatever u provide into args
+        *
+        * while compiling without IDE you can give arguments in the command line interface also
+        * eg: javac Recursion.java
+            java Recursion 1 2 3 "Testing the main method"*/
+        for(String eachArgument : args){
+            System.out.println(eachArgument);
+        }
+        //main(args);
     }
 }
