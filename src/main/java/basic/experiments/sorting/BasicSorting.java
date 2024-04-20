8cb140d1ec80ad585bb56e0534a0335b21b6f686
BattaMahendra <BMC26G@GMAIL.COM>
2024-04-20 15:30:54 +0530

    Brute Force finding min&max in Array

--- src/main/java/basic/experiments/sorting/BasicSorting.java
+++ src/main/java/basic/experiments/sorting/BasicSorting.java
@@ -5,11 +5,29 @@ import java.util.Arrays;
 public class BasicSorting {
 
     public static void main(String[] args) {
-        int[] arr = {4,3,2,1,1,0};
+        int[] arr = {6};
+        sortArray(arr);
+
+        findMinMaxInArray(arr);
+    }
+
+    private static void findMinMaxInArray(int[] arr) {
+        int min = arr[0];
+        int max = 0;
+        for (int i: arr) {
+            min = Math.min(min,i);
+            max = Math.max(max, i);
+        }
+
+        System.out.println("Minimum number in array is "+ min);
+        System.out.println("Maximum number in array is "+ max);
+    }
+
+    private static void sortArray(int[] arr) {
         int counter =0;
 
-        for(int i =0; i< arr.length; i++){
-            for (int j=0; j<arr.length; j++){
+        for(int i = 0; i< arr.length; i++){
+            for (int j = 0; j< arr.length; j++){
                 if(arr[i] > arr[j]){
                     int temp = arr[j];
                     arr[j] = arr[i];
