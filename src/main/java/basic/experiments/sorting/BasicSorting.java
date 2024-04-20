a219e325c83c09ecf8e1a85abc9ac3fc85354b06
BattaMahendra <BMC26G@GMAIL.COM>
2024-04-20 15:56:34 +0530

    Selection sort algorithm

--- src/main/java/basic/experiments/sorting/BasicSorting.java
+++ src/main/java/basic/experiments/sorting/BasicSorting.java
@@ -5,10 +5,41 @@ import java.util.Arrays;
 public class BasicSorting {
 
     public static void main(String[] args) {
-        int[] arr = {6};
+        int[] arr = {6,5,3,6,3,4,2,1};
         sortArray(arr);
 
         findMinMaxInArray(arr);
+
+        selectionSort(arr);
+    }
+
+    private static void selectionSort(int[] arr) {
+
+        System.out.println("\nBefore selection sort :"+ Arrays.toString(arr));
+
+        for(int i = 0; i< arr.length; i++){
+
+            /*
+            * Finding minimum number index between i and n-1
+            * Then store the index in min variable*/
+            int min =i;
+            for(int j = i; j< arr.length; j++){
+                if(arr[j] < arr[i] ) min = j;
+            }
+            /*
+            * After finding minimum number index , swapping it with index i */
+            int temp = arr[i] ;
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        System.out.println("After selection sort :"+ Arrays.toString(arr));
+    }
+
+    private static void swap(int i , int j){
+        i = i + j;
+        j = i - j;
+        i = i - j;
     }
 
     private static void findMinMaxInArray(int[] arr) {
