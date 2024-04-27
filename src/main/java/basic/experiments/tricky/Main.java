735d41fbef1e11a9d466d6abf9c881a6352734fe
BattaMahendra <BMC26G@GMAIL.COM>
2024-04-27 16:23:15 +0530

    Object recursion stack overflow error

---
+++ src/main/java/basic/experiments/tricky/Main.java
@@ -0,0 +1,25 @@
+package basic.experiments.tricky;
+
+import lombok.ToString;
+
+@ToString
+public class Main {
+
+    public Main ref;
+
+    public String name;
+
+    public Main(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Main m1 = new Main("one");
+        Main m2 = new Main("two");
+
+        m1.ref = m2;
+        m2.ref = m1;
+        System.out.println(m1);
+
+    }
+}
