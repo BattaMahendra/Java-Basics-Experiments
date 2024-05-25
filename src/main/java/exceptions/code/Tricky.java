2d1f0a5dec9095bf9f565f0c1862babfc3e4fee4
BattaMahendra <BMC26G@GMAIL.COM>
2024-05-25 19:32:02 +0530

    Learning java exceptions

---
+++ src/main/java/exceptions/code/Tricky.java
@@ -0,0 +1,31 @@
+package exceptions.code;
+
+public class Tricky {
+
+    public static void main(String[] args) {
+
+        int i = tricky();
+        System.out.println(i);
+
+    }
+
+
+    // change string s value to null and Mahendra to see changes
+
+    private static int tricky() {
+        try{
+            System.out.println("we are in the try block");
+            //String s = "Mahendra";
+            String s = null;
+            return s.length();
+        }catch(Exception e) {
+            System.out.println(" we are in the catch block \n");
+            return 0;
+        }
+        finally{
+            //System.exit(0);
+            System.out.println(" we are in the finally block \n");
+            //return 1;
+        }
+    }
+}
