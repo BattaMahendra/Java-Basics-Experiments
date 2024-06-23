26aa4f259120f3461c6dcfcf24f5c4f1187a2912
BattaMahendra <BMC26G@GMAIL.COM>
2024-06-23 17:21:37 +0530

    Solving some stream problems

--- src/main/java/java8/Java_Streams/Stream_Problems.java
+++ src/main/java/java8/Java_Streams/Stream_Problems.java
@@ -19,11 +19,7 @@ public class Stream_Problems {
 
         duplicatesInList(list);
 
-       Integer max = list.stream().max(Comparator.comparing(Integer::valueOf)).orElse(-1);
-
-       Integer min = list.stream().min((i,j)->  i-j).orElse(-1);
-
-        System.out.println("Max and min values in list: "+max+" "+ min);
+        minAndMax(list);
 
         //Consider a array containing 0's and 1's and now move all 1's to right and zeros to left using java 8
         Integer[] arr = {1,0,0,1,0,1,1,0,0};
@@ -51,6 +47,193 @@ public class Stream_Problems {
          */
         characterFrequency();
 
+        //sort the following list
+
+        sortingGivenList(list);
+
+        /**
+         * Join a list of strings with '[' as prefix, ']' as suffix, and ',' as delimiter
+         *
+         * Given a list of strings, write a Java 8 program to join the strings
+         * with '[' as a prefix, ']' as a suffix, and ',' as a delimiter.
+         */
+
+        joinListOfStrings();
+
+        /**
+         * Check if two strings are anagrams or not using Java 8 streams
+         * Write a Java 8 program to check if two strings are anagrams or not using the stream API and lambda expressions.
+         */
+        String first = "listen";
+        String second = "silent";
+
+        //using basic hashing approach
+        boolean value =  hashingApproachForAnagram(first, second);
+
+        //using java 8
+        isAnagramUsingJava8(first, second);
+
+        /**
+         * Find the sum of all digits of a number in Java 8
+         *
+         * Write a Java 8 program to find the sum of all digits of a given number.
+         *
+         */
+        int num = 12345;
+        //basic approach
+        basicSum(num);
+        // Using Java 8 stream to sum up the digits
+        int sum = String.valueOf(Math.abs(num)).chars()
+                .map(Character::getNumericValue)
+                .sum();
+        /**
+         * Sort a list of strings according to the increasing order of their length
+         *
+         * Write a Java 8 program to sort a given list of strings according to the increasing order of their length.
+         */
+        sortByLengthOfStringList();
+
+        /**
+         * Find the common elements between two arrays
+         *
+         * Write a Java 8 program to find the common elements between two arrays using streams.
+         */
+        String[] words1 = {"hey", "hello", "Hii", "give", "damn", "said"};
+        String[] words2 = {"hey", "how", "Hii", "give", "what", "said"};
+
+        //using list internal methods
+        List<String> wordList1 = Arrays.asList(words1);
+        List<String> wordList2 = Arrays.asList(words2);
+
+        for(String s: wordList1){
+            wordList2.contains(s);
+        }
+
+        String s = "helohellok";
+
+        //first repeated character in a string
+        System.out.println("\nFirst repeated character in given string is "+ firstRepeatedChar(s));
+
+        //first non-repetitive character or first unique character in a string
+
+        usingStringInternalFunctions(s);
+
+
+    }
+
+    private static void usingStringInternalFunctions(String s) {
+        //using string internal methods
+        for (char c : s.toCharArray()) {
+            if(s.indexOf(c) == s.lastIndexOf(c)){
+                System.out.println("The first non repeated  character is "+c);
+                break;
+            }
+        }
+
+    }
+
+    private static Character firstRepeatedChar(String s) {
+        Set<Character> charSet = new HashSet<>();
+        for(char c : s.toCharArray()){
+            if(charSet.add(c) != false) return c;
+        }
+        return null;
+    }
+
+    private static void sortByLengthOfStringList() {
+        List<String> names = Arrays.asList("rohit", "urmila", "rohit", "urmila", "ram", "sham", "sita", "gita");
+        names.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .forEach(System.out::println);
+    }
+
+
+    private static void basicSum(int num) {
+        int sum = 0;
+        while(num !=0){
+            sum = sum + num %10;
+            num = num /10;
+        }
+        System.out.println("\nSum of digits in given number is: "+sum);
+    }
+
+    private static void isAnagramUsingJava8(String first, String second) {
+        char[] f = first.toCharArray();
+        char[] s = second.toCharArray();
+
+        //sorting arrays
+        Arrays.sort(f);
+        Arrays.sort(s);
+
+        //method 1: comparing arrays
+        if( Arrays.equals(f,s)) System.out.println("is anagram");
+        else System.out.println("not anagram");
+
+        //method 2: using string
+        boolean result = Arrays.toString(f).equals(Arrays.toString(s)) ? true : false;
+        System.out.println(result);
+    }
+
+    private static boolean hashingApproachForAnagram(String first, String second) {
+        int[] hashArray = new int[256];
+        if(first.length()!=second.length()) return false;
+        //populating the hashArray with first word and second word
+        for(int i = 0; i < first.length(); i++) {
+            hashArray[first.charAt(i)]++;
+            hashArray[second.charAt(i)]--;
+        }
+
+        for(int i=0; i< first.length(); i++){
+            if( hashArray[first.charAt(i)] != 0 || hashArray[second.charAt(i)] != 0) return false;
+        }
+
+        return true;
+
+    }
+
+    private static void minAndMax(List<Integer> list) {
+        Integer max = list.stream().max(Comparator.comparing(Integer::valueOf)).orElse(Integer.MAX_VALUE);
+
+        //using Integer::compareTo
+        Integer max2 = list.stream().max(Integer::compareTo).orElse(Integer.MAX_VALUE);
+        Integer min = list.stream().min((i, j)->  i-j).orElse(-1);
+
+        System.out.println("Max and min values in list: "+max+" "+ min);
+
+        System.out.println("\nUsing IntSummaryStatistics");
+        IntSummaryStatistics summaryStatistics = list.stream()
+                .collect(summarizingInt(Integer::intValue));
+        System.out.println(summaryStatistics.getMax());
+        System.out.println(summaryStatistics.getMin());
+        System.out.println(summaryStatistics.getCount());
+        System.out.println(summaryStatistics.getSum());
+        System.out.println(summaryStatistics.getAverage());
+    }
+
+    private static void joinListOfStrings() {
+        List<String> languageList = Arrays.asList("java", "c++", "c", "C sharp", "python", "kotlin", "scala");
+        String joinWithPrefixSuffixAndDelimiter = languageList
+                .stream()
+                .collect(joining(",", "[", "]"));
+        System.out.println("\nAfter joing the given array list with prefix: [ and suffix: ] and delimiter: , is \n"+joinWithPrefixSuffixAndDelimiter);
+    }
+
+
+    private static void sortingGivenList(List<Integer> list) {
+        System.out.println("\nSorting a given list");
+        List<Integer> sortedList = list.stream().sorted().collect(toList());
+        System.out.println("Using sorted() : "+sortedList);
+
+        sortedList = list.stream().sorted(Comparator.reverseOrder()).collect(toList());
+        System.out.println("reverse sorted: "+sortedList);
+
+        sortedList = list.stream().sorted((i, j) -> i - j).collect(toList());
+        /*
+        * if i-j > 0 then j comes first
+        * if i-j == 0 then no change
+        * if i-j < 0 then i comes first
+        * */
+        System.out.println("using customComparator : "+sortedList);
     }
 
     private static void characterFrequency() {
