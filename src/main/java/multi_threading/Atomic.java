a5601916e7661da068d89b850c0d8c4a4798994c
BattaMahendra <BMC26G@GMAIL.COM>
2024-07-14 10:40:03 +0530

    Revising multi-threading

---
+++ src/main/java/multi_threading/Atomic.java
@@ -0,0 +1,52 @@
+package multi_threading;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Atomic {
+
+   static int unsafeCounter =0;
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    static volatile  int volatileInteger =0;
+
+    /*
+    * You can observe by running below program only atomic will give you accurate results
+    * You can still use synchronized instead of Atomic but it is more costly as it locks and suspends threads
+    * So Atomic variables are always preferred
+    *
+    * here the volatile fails because of race condition between threads
+    * go and read more about it in the Theory file in this package
+    * */
+    static void process() {
+
+        int max = 1_000_00_000;
+
+        for (int i = 0; i <= max; i++) {
+            unsafeCounter++;
+            atomicInteger.getAndIncrement();
+            volatileInteger++;
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(Atomic::process);
+        Thread t2 = new Thread(Atomic::process);
+        Thread t3 = new Thread(Atomic::process);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("Normal integer after operation : "+unsafeCounter);
+        System.out.println("Atomic integer after operation : "+atomicInteger);
+        System.out.println("Volatile integer after operation : "+volatileInteger);
+
+    }
+
+}
+
+
