67e51ab98768b4545e6bf7196bf252e3f09df4bf
BattaMahendra <BMC26G@GMAIL.COM>
2024-08-08 18:41:36 +0530

    Creating custom hashmap

--- src/main/java/collections/maps/CustomMap.java
+++ src/main/java/collections/maps/CustomMap.java
@@ -1,8 +1,38 @@
 package collections.maps;
 
+import java.util.Objects;
+
 public class CustomMap<K,V> {
 
-    private Node[] bucketArray = new Node[16];
+    private int threshold =0;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int defaultSize;
+    private Node[] bucketArray ;
+
+    public CustomMap() {
+        defaultSize = 16;
+        bucketArray = new Node[defaultSize];
+    }
+
+    public CustomMap(int initialCapacity){
+        defaultSize = initialCapacity;
+        bucketArray = new Node[defaultSize];
+    }
+
+    public void resize(){
+        Node[] oldNodeArray = bucketArray;
+
+        bucketArray = new Node[oldNodeArray.length * 2];
+
+        //copying old array contents into this new bucket
+        for(Node<K,V> node : oldNodeArray) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
 
     static class Node<K,V>{
         public K key;
@@ -16,36 +46,48 @@ public class CustomMap<K,V> {
     }
 
     public int hash(K key){
-        int hashRange = 16;
-        return key.hashCode() % hashRange;
+        int hashRange = bucketArray.length;
+        return Objects.hash(key) % hashRange; //simple hashing mechanism by % operator
     }
 
-    public void put(K key , V value){
+    public V put(K key , V value){
+
+        //calculate threshold and resize array if required
+        if (threshold >= LOAD_FACTOR * bucketArray.length) {
+            resize();
+        }
+
         int nodeValue = hash(key);
         Node presentNode = bucketArray[nodeValue];
 
-        if(presentNode == null){
+        if(presentNode == null){      // check if the node of the bucket is empty
             Node newNode = new Node<>(key, value);
-            bucketArray[nodeValue] = newNode;
+            bucketArray[nodeValue] = newNode; //then add new node to the bucket
+            threshold++;
         }else{
             Node prevNode = presentNode;
 
-            while(presentNode != null){
-                if(presentNode.key.equals(key)){
-                    presentNode.value = value;
-                    return;
+            while(presentNode != null){   //traverse all the linked nodes to find the last node
+                if(presentNode.key.equals(key)){   //check if any linked nodes's key matches with given key
+                    V returnableValue = (V)presentNode.value;
+                    presentNode.value = value;  // update the value of present node and return old value
+                    return returnableValue;
                 }
 
                 prevNode = presentNode;
-                presentNode = presentNode.next;
+                presentNode = presentNode.next; // keep on traversing upto  last node
             }
             Node newNode = new Node<>(key, value);
             prevNode.next = newNode;
+            threshold++;
+            return null;
         }
+
+        return null;
     }
 
     public V get(K key){
-        int hashCode = hash(key);
+        int hashCode = hash(key);  // calculate hash of the given key
         Node presentNode = bucketArray[hashCode];
 
         while(presentNode != null){
@@ -56,6 +98,35 @@ public class CustomMap<K,V> {
             presentNode = presentNode.next;
         }
 
+        return null;
+    }
+
+    public V remove (K key){
+
+        int hash = hash(key);
+        Node head = bucketArray[hash];
+
+        if(head == null) return null;
+
+        Node presentNode = head.next;
+        Node prevNode = null;
+        while( presentNode!= null){
+
+            if(presentNode.key.equals(key)){
+                if(prevNode == null) {
+                   bucketArray[hash] = presentNode.next; //remove head node
+                }
+                else{
+                    prevNode = presentNode.next; //remove any middle node
+                }
+
+                threshold--;
+                return (V)presentNode.value;
+            }
+            prevNode = presentNode;
+            presentNode = presentNode.next;
+
+        }
         return null;
     }
 
--- src/main/java/collections/maps/CustomMapTest.java
+++ src/main/java/collections/maps/CustomMapTest.java
@@ -1,5 +1,7 @@
 package collections.maps;
 
+import java.util.Objects;
+
 public class CustomMapTest {
 
     public static void main(String[] args) {
@@ -11,12 +13,21 @@ public class CustomMapTest {
         System.out.println(map);
 
         CustomMap<Integer , Integer> map2 = new CustomMap<>();
+        String s= null;
+        Integer i = null;
+        System.out.println(Objects.hash(s));
+        System.out.println(Objects.hash(i));
         map2.put(16,16);
         map2.put(32,16);
         map2.put(48,16);
         map2.put(64,16);
         map2.put(80,16);
         map2.put(48,17);
+        map2.put(3,17);
+        map2.put(null,17);
+        map2.remove(80);
         System.out.println(map2);
+
+        System.out.println(map2.get(49));
     }
 }
