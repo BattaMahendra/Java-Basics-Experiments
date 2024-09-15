07ac59ed06c4fb47e66b8c1a2f104a3127090186
BattaMahendra <BMC26G@GMAIL.COM>
2024-09-15 18:36:21 +0530

    Learning more about interfaces

--- src/main/java/oops/abstraction/IProcessor.java
+++ src/main/java/oops/abstraction/IProcessor.java
@@ -9,6 +9,12 @@ public interface IProcessor extends Serializable , Cloneable {
     /*Compiler by default convert below line into public abstract void process();*/
     void process();
 
+
+/*
+*  1. Default methods provide a default behavior that can be overridden.
+   2. Useful when adding methods to existing interfaces without breaking existing implementations.
+   3. They can be overridden, unlike static methods.
+* */
     default void utilizeCores(){
         System.out.println("Utilizing the cores from IProcessor");
     }
---
+++ src/main/java/oops/abstraction/interfaces/Main.java
@@ -0,0 +1,50 @@
+package oops.abstraction.interfaces;
+
+interface Logger {
+    // Default method that uses private method
+    default void logInfo(String message) {
+
+        log("INFO", message);
+    }
+
+    // Default method that uses private method
+    default void logError(String message) {
+
+        log("ERROR", message);
+    }
+
+    // Private method in interface (used by default methods)
+    private void log(String level, String message) {
+
+        System.out.println("[" + level + "]: " + message);
+    }
+
+    //static methods - used for utility functions
+    public static void printHeader(){
+
+        //static method is using private static methods
+        //private default methods are not allowed here
+        print("Started");
+    }
+
+    public static void printFooter(){
+        print("ended");
+    }
+
+    //private static methods - used to maintain reoccurring functionality of static methods
+    private static void print(String term){
+        System.out.println("Logger functionality: "+ term);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger.printHeader();
+        Logger logger = new Logger() {}; // Anonymous class
+        logger.logInfo("System is starting"); // Output: [INFO]: System is starting
+        logger.logError("An error occurred"); // Output: [ERROR]: An error occurred
+
+        Logger.printFooter();
+    }
+}
