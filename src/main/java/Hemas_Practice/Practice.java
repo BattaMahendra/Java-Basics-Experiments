82dd32956795fb95423d198cb914267bfbb500a4
batta.chowdary <bmc26g@gmail.com>
2024-11-08 21:26:59 +0530

    Hema Prep - Duplicates Problem

---
+++ src/main/java/Hemas_Practice/Practice.java
@@ -0,0 +1,13 @@
+package Hemas_Practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(0,1,1,1,1,7,8,9,5,2,36,4,78,222,24,9);
+
+        //find the duplicates in the above list
+    }
+}
