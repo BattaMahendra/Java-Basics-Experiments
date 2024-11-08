4d23a4f46de761577ec90ea17f56233fffd01872
batta.chowdary <bmc26g@gmail.com>
2024-11-08 21:37:45 +0530

    Hema Prep - Duplicates Problem

--- src/main/java/Hemas_Practice/Practice.java
+++ src/main/java/Hemas_Practice/Practice.java
@@ -1,6 +1,7 @@
 package Hemas_Practice;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Practice {
@@ -9,5 +10,27 @@ public class Practice {
         List<Integer> list = Arrays.asList(0,1,1,1,1,7,8,9,5,2,36,4,78,222,24,9);
 
         //find the duplicates in the above list
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        //Hashset stores only unique values
+        //if a value is unique it returns true
+        //if that value is already present in the hashset then it returns false
+        for(int i=0; i<list.size(); i++){
+
+        }
+
+        //List internal methods to find out duplicates
+
+        for(int i=0; i<list.size(); i++){
+
+            int value = list.get(i);
+
+            if( list.indexOf(value) != list.lastIndexOf(value)){
+                System.out.println(value);
+            }
+
+        }
+
+
     }
 }
